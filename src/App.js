15dcf2fba43dab9ca0909fc5ad88362fa6044b04
Daja Mitchner <daja.mitchner@yahoo.com>
2021-03-17 17:54:43 -0400

    wed assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import './App.css';
 
+const App extends React.Component{
+  constructor(props){
+    super(props);
+  };
+  this.state = {
+    searchTerm: 'React',
+  };
+
+render(){
+  const {searchTerm} = this.state;
+
+return (
+  <div>
+    <h1>My Hacker Stories</h1>
+
+    <SearchForm
+    searchTerm={searchTerm}
+    onSearchInput={()=> this.setState({
+      searchTerm: event.target.value
+    })}
+    />
+  </div>
+)
+}
+}
+
+
+
+
+
 // const words = [
 //   'spray',
 //   'limit',
@@ -13,69 +43,69 @@ import './App.css';
 //   })
 // console.log(filteredWords)
 
-  const App = () => {
-    const stories = [
-      {
-        title: 'React',
-        url:'https://react.js.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-      },
-      {
-        title: 'Redux',
-        url:'https://redux.js.org/',
-        author:'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-      },
-    ];
+//   const App = () => {
+//     const stories = [
+//       {
+//         title: 'React',
+//         url:'https://react.js.org/',
+//         author: 'Jordan Walke',
+//         num_comments: 3,
+//         points: 4,
+//         objectID: 0,
+//       },
+//       {
+//         title: 'Redux',
+//         url:'https://redux.js.org/',
+//         author:'Dan Abramov, Andrew Clark',
+//         num_comments: 2,
+//         points: 5,
+//         objectID: 1,
+//       },
+//     ];
   
-    // const List = ({list}) => 
-    // list.map(item => <Item key={item.objectID} item={item} />);
-    // const Item = ({item}) => (
-    //   <div>
-    //     <span>
-    //       <a href={item.url}>{item.title}</a>
-    //     </span>
-    //     <span>{item.author}</span>
-    //     <span>{item.num_comments}</span>
-    //     <span>{item.points}</span>
-    //   </div>
-    // )
+//     // const List = ({list}) => 
+//     // list.map(item => <Item key={item.objectID} item={item} />);
+//     // const Item = ({item}) => (
+//     //   <div>
+//     //     <span>
+//     //       <a href={item.url}>{item.title}</a>
+//     //     </span>
+//     //     <span>{item.author}</span>
+//     //     <span>{item.num_comments}</span>
+//     //     <span>{item.points}</span>
+//     //   </div>
+//     // )
    
-    // const handleSearch = event => {
-    //  console.log(event.target.value)
-    // }
+//     // const handleSearch = event => {
+//     //  console.log(event.target.value)
+//     // }
     
-    const handleFetchStories = React.useCallback(() => {
-      if (!searchTerm) return;
+//     const handleFetchStories = React.useCallback(() => {
+//       if (!searchTerm) return;
 
-      dispatchStories({ type : 'STORIES_FETCH_INIT' });
+//       dispatchStories({ type : 'STORIES_FETCH_INIT' });
 
-      fetch(`${API_ENDPOINT}${SEARCHTERM}`)
-      .then(response => response.json())
-      .then(result => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
-        }),
-      }),
-      .catch(() =>
-      dispatchStories({type: 'STORIES_FETCH_FAILURE'});
-      );
-    }, [searchTerm]);
-  React.useEffect(() => {
-    handleFetchStories();
-  }, [handleFetchStories]);
-};
-  // const Search = props => (
-  //   <div> 
-  //    <label htmlFor="search">Search: </label>
-  //    <input id='search' type='text' onChange={props.onSearch}/>
-  //   </div>
-  // )
+//       fetch(`${API_ENDPOINT}${SEARCHTERM}`)
+//       .then(response => response.json())
+//       .then(result => {
+//         dispatchStories({
+//           type: 'STORIES_FETCH_SUCCESS',
+//           payload: result.hits,
+//         }),
+//       }),
+//       .catch(() =>
+//       dispatchStories({type: 'STORIES_FETCH_FAILURE'});
+//       );
+//     }, [searchTerm]);
+//   React.useEffect(() => {
+//     handleFetchStories();
+//   }, [handleFetchStories]);
+// };
+//   // const Search = props => (
+//   //   <div> 
+//   //    <label htmlFor="search">Search: </label>
+//   //    <input id='search' type='text' onChange={props.onSearch}/>
+//   //   </div>
+//   // )
   
   export default App;
